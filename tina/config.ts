9e3395de6df80300f471f63d30eb5264f81ee337
lukerall <lukederek105@gmail.com>
2026-03-30 07:54:30 +0200

    Connect Tina Cloud credentials

--- tina/config.ts
+++ tina/config.ts
@@ -2,18 +2,17 @@ import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
   process.env.HEAD ||
-  "main";
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  "master";
 
 export default defineConfig({
   branch,
 
   // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID || "",
   // Get this from tina.io
-  token: process.env.TINA_TOKEN,
+  token: process.env.TINA_TOKEN || "",
 
   build: {
     outputFolder: "admin",
